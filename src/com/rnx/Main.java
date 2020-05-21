3ab6318d7467173dab4aff68056212dbdbb39da5
Nanlung <Nanlung.temlong@gmail.com>
2020-05-21 23:53:45 +0100

    Initial Commit

---
+++ src/com/rnx/Main.java
@@ -0,0 +1,18 @@
+package com.rnx;
+/*
+Multiplication Table using nested loops
+
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=12 ; ++i) {
+            for (int j = 1; j <= 12; ++j)
+                System.out.println((i * j) + " ");
+
+            System.out.println();
+        }
+
+
+    }
+}
